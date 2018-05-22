0f608e46a17c30d4186f8d03a74f580300578101
sebj <sebbocaj@yahoo.fr>
2018-05-22 13:53:39 +0200

    Improvements

--- imports/Summary_expenses/Create_expense.js
+++ imports/Summary_expenses/Create_expense.js
@@ -37,9 +37,9 @@ export default class Create_expense extends React.Component{
                                 </p>
                         </div>
 
-                
+                                        <p>Choose a Category</p>
+
                     <div onClick={this.ChooseCategory.bind(this)}>
-                    <p>Choose a category</p>
                         <img name="Restaurant" src="http://res.cloudinary.com/dygu6sw0x/image/upload/v1524498150/restaurant.png"/>
                         <img name="Transport" src="http://res.cloudinary.com/dygu6sw0x/image/upload/v1524499475/transport.png"/>
                         <img name="Hotel" src="http://res.cloudinary.com/dygu6sw0x/image/upload/v1524499531/hotel.png"/>
